bb9743c4d4155706039638265af7b392f2b4357c
azzam <azzam.shaikh8@gmail.com>
2024-02-17 11:47:31 -0600

    Updated taxi service to go to all pick up points then drop off. Revised pickup location vector to be a struct. Added timing info

--- src/taxi_service/src/taxi_service.cpp
+++ src/taxi_service/src/taxi_service.cpp
@@ -6,18 +6,7 @@
 #include <array>
 #include <algorithm>
 #include <random>
-
-// Define global pickup and dropoff variables
-// float siteA[3] = {0, 3.5, 1.0};
-// float siteB[3] = {6.0, 2, 1.0};
-// float siteC[3] = {4.5, 4, 1.0};
-// float siteD[3] = {-0.75, -5, 1.0};
-// float siteE[3] = {-6, 2.5, 1.0};
-// bool arrived_at_pickup = true;
-// float dropoff[3] = {-2, 5, 1.0};
-// bool arrived_at_dropoff = true;
-
-// float sites[5] = {siteA,siteB,siteC,siteD,siteE};
+#include <chrono>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -25,49 +14,49 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 class TaxiService{
 
   private:
-    float siteA[3] = {0, 3.5, 1.0};
-    float siteB[3] = {6.0, 2, 1.0};
-    float siteC[3] = {4.5, 4, 1.0};
-    float siteD[3] = {-0.75, -5, 1.0};
-    float siteE[3] = {-6, 2.5, 1.0};
-    //float sites[5] = {siteA,siteB,siteC,siteD,siteE};
-
-    std::vector<std::array<float,3>> testsites;
-    std::vector<std::array<float,3>> pickUpSites;
-
-
-
-
+    // float siteA[3] = {0, 3.5, 1.0};
+    // float siteB[3] = {6.0, 2, 1.0};
+    // float siteC[3] = {4.5, 4, 1.0};
+    // float siteD[3] = {-0.75, -5, 1.0};
+    // float siteE[3] = {-6, 2.5, 1.0};
+    // float sites[5] = {siteA,siteB,siteC,siteD,siteE};
+    
+    
     float dropoff[3] = {-1.8, 6.8, 1};
     bool arrived_at_pickup = true;
     bool arrived_at_dropoff = true;
 
-    
-
     ros::Publisher goal_pub;
-
     std_msgs::Bool reached;
-
     move_base_msgs::MoveBaseGoal goal;
-
-    //actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-
-    unsigned seed = 0;
     
 
-
+    struct Site {
+      std::vector<float> siteLocation;
+      std::string siteName;
+    };
+    std::vector<Site> testsites;
+    std::vector<Site> pickUpSites;
+    
 
   public:
     TaxiService(ros::NodeHandle *nh){
       goal_pub = nh->advertise<std_msgs::Bool>("reached_goal", 10);
-      testsites.push_back({-0.5, 2.5, 1.0});
-      testsites.push_back({6.0, 2, 1.0});
+      
+      struct Site siteA = { {-0.5, 2.5, 1.0}, "Site A"};
+      struct Site siteB = {{ 6.0, 2, 1.0}, "Site B"};
+      struct Site siteC = {{4.5, -4, 1.0}, "Site C"};
+      struct Site siteD = {{-0.75, -7, 1.0}, "Site D"};
+      struct Site siteE = {{-6, 2.5, 1.0}, "Site E"};
+      
+      testsites.push_back(siteA);
+      testsites.push_back(siteB);
 
-      pickUpSites.push_back({-0.5, 2.5, 1.0});
-      pickUpSites.push_back({6.0, 2, 1.0});
-      pickUpSites.push_back({4.5, -4, 1.0});
-      pickUpSites.push_back({-0.75, -7, 1.0});
-      pickUpSites.push_back({-6, 2.5, 1.0});
+      pickUpSites.push_back(siteA);
+      pickUpSites.push_back(siteB);
+      pickUpSites.push_back(siteC);
+      pickUpSites.push_back(siteD);
+      pickUpSites.push_back(siteE);
 
     }
 
@@ -86,13 +75,21 @@ class TaxiService{
 
       // shuffle test sites
 
-      std::shuffle(pickUpSites.begin(), pickUpSites.end(), std::default_random_engine());
+      std::shuffle(pickUpSites.begin(), pickUpSites.end(), std::random_device());
+      std::cout << "The pickup site sequence is as follows:" << std::endl;
 
       for (auto& site : pickUpSites){
-        goal.target_pose.pose.position.x = site[0];
-        goal.target_pose.pose.position.y = site[1];
-        goal.target_pose.pose.orientation.w = site[2];
-    
+        std::cout << site.siteName << std::endl;
+      }
+
+      const auto taxiStartTime{std::chrono::steady_clock::now()};
+      for (auto& site : pickUpSites){
+        const auto siteStartTime{std::chrono::steady_clock::now()};
+        
+        goal.target_pose.pose.position.x = site.siteLocation[0];
+        goal.target_pose.pose.position.y = site.siteLocation[1];
+        goal.target_pose.pose.orientation.w = site.siteLocation[2];
+        std::cout << "Going to " << site.siteName << std::endl;
 
         // Send the goal position and orientation for the robot to reach
         ROS_INFO("Sending the robot the pickup location!");
@@ -103,6 +100,12 @@ class TaxiService{
         ac.waitForResult();
         if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
           ROS_INFO("Hooray, the robot is at the pickup location!");
+          const auto siteEndTime{std::chrono::steady_clock::now()};
+          const std::chrono::duration<double> site_elapsed_seconds{siteEndTime-siteStartTime};
+          std::cout << "It took " 
+                    << site_elapsed_seconds.count()
+                    << " seconds to reach this site." 
+                    << std::endl;
           //ROS_INFO("Publishing pickup message!");
 
           // Publish data 
@@ -119,48 +122,55 @@ class TaxiService{
           // Change status to false
           reached.data = false;
 
-          // --- DROPOFF LOCATION ---
 
-          // Define a position and orietnation for the robot to reach
-          goal.target_pose.pose.position.x = dropoff[0];
-          goal.target_pose.pose.position.y = dropoff[1];
-          goal.target_pose.pose.orientation.w = dropoff[2];
-
-          // Send the goal position and orientation for the robot to reach
-          ROS_INFO("Sending the robot the dropoff location!");
-          ac.sendGoal(goal);
-          ROS_INFO("Robot is on the way!");
-
-          // Wait an infinite time for the results
-          ac.waitForResult();
-
-          // Check if the robot has reached its goal
-          if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-            ROS_INFO("Hooray, the robot is at the dropped off location!");
-            //ROS_INFO("Publishing dropoff message!");
-            ROS_INFO("Dropping the object right now...");
-            // Publish data
-            reached.data = true;
-            goal_pub.publish(reached);
-            ROS_INFO("Dropoff Complete!");
-
-            /*
-            if(reached.data == true){
-              ROS_INFO("---SHOULD HAVE PUBLISHED DATA---");
-            }*/
-
-            ROS_INFO("--- Pickup and dropoff complete! ---");
-            ros::Duration(5.0).sleep();
-
-            
-          }
-          else{
-            ROS_INFO("The base failed to move to the drop off location for some reason.");
-          }
         }
         else{
           ROS_INFO("The base failed to move to the pick up location for some reason.");
         }
+      }
+
+      ROS_INFO("All waste has been picked up!"); 
+       
+      // --- DROPOFF LOCATION ---
+
+      // Define a position and orietnation for the robot to reach
+      goal.target_pose.pose.position.x = dropoff[0];
+      goal.target_pose.pose.position.y = dropoff[1];
+      goal.target_pose.pose.orientation.w = dropoff[2];
+
+      // Send the goal position and orientation for the robot to reach
+      ROS_INFO("Sending the robot the dropoff location!");
+      ac.sendGoal(goal);
+      ROS_INFO("Robot is on the way!");
+
+      // Wait an infinite time for the results
+      ac.waitForResult();
+
+      // Check if the robot has reached its goal
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+        ROS_INFO("Hooray, the robot is at the dropped off location!");
+        const auto taxiEndTime{std::chrono::steady_clock::now()};
+        const std::chrono::duration<double> taxi_elapsed_seconds{taxiEndTime-taxiStartTime};
+        std::cout << "It took " << taxi_elapsed_seconds.count() << " seconds to complete the Taxi service!" << std::endl;
+
+        //ROS_INFO("Publishing dropoff message!");
+        ROS_INFO("Dropping the object right now...");
+        // Publish data
+        reached.data = true;
+        goal_pub.publish(reached);
+        ROS_INFO("Dropoff Complete!");
+
+        /*
+        if(reached.data == true){
+          ROS_INFO("---SHOULD HAVE PUBLISHED DATA---");
+        }*/
+
+        ROS_INFO("--- Pickup and dropoff complete! ---");
+        ros::Duration(5.0).sleep();
+        
+      }
+      else{
+        ROS_INFO("The base failed to move to the drop off location for some reason.");
       }
 
       return true;
